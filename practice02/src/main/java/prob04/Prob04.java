f746a6113b59bc71e540f75eda9f44c2d78258a0
hanjoohyung <ghd167@naver.com>
2021-09-09 20:13:34 +0900

    09/09

--- practice02/src/main/java/prob04/Prob04.java
+++ practice02/src/main/java/prob04/Prob04.java
@@ -1,4 +1,5 @@
 package prob04;
+
 public class Prob04 {
 
 	public static void main(String[] args) {
@@ -8,14 +9,14 @@ public class Prob04 {
 		char[] c2 = reverse( "Java Programming!" );
 		printCharArray( c2 );
 	}
-	
 	public static char[] reverse(String str) {
-		
-		return null;
+		char[] data=new char[str.length()]; //문자열을 담을 배열
+		for(int i =str.length()-1;i>=0;i--) { // 문자열을 한글자씩 끊어 배열에 저장
+			data[str.length()-1-i]=str.charAt(i);
+		}
+		return data;
 	}
-
-	public static void printCharArray(char[] array){
-	
+	public static void printCharArray(char[] array){	
 		System.out.println( array );
 	}
 }
--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -4,23 +4,22 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Prob05 {
-
 	public static void main(String[] args) {
 	
-	
 		Scanner scanner = new Scanner( System.in );
-		System.out.println("수를 결정하였습니다. 맞추어 보세요");
 
 		while( true ) {
 			
 			/* 게임 작성 */
-			System.out.println("1-100");
+			int min = 1;
+			int max = 100;
 			// 정답 램덤하게 만들기
-			
 			Random random = new Random();
-			int correctNumber = random.nextInt( 100 ) + 1;
+			System.out.println("수를 결정하였습니다. 맞추어 보세요");
+			int correctNumber = random.nextInt( 100 ) + 1; // Random class로 1-100까지의 임의의 정수를 랜덤으로 생성
 			System.out.println(correctNumber);
-			for(int j=1;j<101;j++) {
+			for(int j=1;;j++) {  // for문의 중간 식을 넣지 않으면 무한대로 돌아감.
+				System.out.println(min + "-"+ max);
 				System.out.print(j+">>");
 				int i = scanner.nextInt();
 				if(i==correctNumber) {
@@ -28,13 +27,13 @@ public class Prob05 {
 					break;
 				}
 				else {
-					if(i>correctNumber) {
+					if(i>correctNumber) { // 입력값이 정답보다 크다면
 						System.out.println("더 낮게");
-						System.out.println();							
+						max = i;  // 범위 최대 값을 i로 지정
 					}
-					else if(i<correctNumber){
+					else if(i<correctNumber){ // 입력값이 정답보다 작다면
 						System.out.println("더 높게");
-						System.out.println();	
+						min = i;  //  범위 최소 값을 i로 지정
 					}
 				}				
 			}
@@ -42,10 +41,13 @@ public class Prob05 {
 			System.out.print( "다시 하겠습니까(y/n)>>" );
 			String answer = scanner.next();
 			if( "y".equals( answer ) == false ) {
+				return;
+			}
+			else {
 				break;
 			}
-			scanner.close();
+			
 		}
-		
+		scanner.close();
 	}
 }
